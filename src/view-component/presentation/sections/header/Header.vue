3b897f8e098b36bbbda0108fbf1598e9274d8bfe
protypangel <mascret@et.esiea.fr>
2025-08-27 19:25:06 +0200

    Change for small height

---
+++ src/view-component/presentation/sections/header/Header.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import HeaderSmallHeight from './HeaderSmallHeight.vue';
+</script>
+
+<template>
+  <section class="relative overflow-hidden">
+    <div class="absolute inset-0 -z-99 pattern-grid opacity-60 pointer-events-none"/>
+    <!-- todo: it's not 200% height -->
+    <div
+      class="
+        relative h-full flex flex-col justify-between
+        px-4 gap-2
+      ">
+      <HeaderSmallHeight class="[@media(--hbase)]:hidden "/>
+    </div>
+  </section>
+</template>
