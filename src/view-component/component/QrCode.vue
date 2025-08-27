3f2514ff946d580c981478ce85d13f28062344e5
protypangel <mascret@et.esiea.fr>
2025-08-28 00:44:39 +0200

    Responsive

--- src/view-component/component/QrCode.vue
+++ src/view-component/component/QrCode.vue
@@ -4,7 +4,7 @@ import QrCodeIcon from '@/assets/QrCodeIcon.svg';
 </script>
 
 <template>
-  <div class="flex h-full">
+  <div class="flex">
     <a
     href="https://www.paypal.com/donate/?hosted_button_id=XYAD7VVFWRTLQ" 
     target="_blank">
@@ -21,7 +21,7 @@ import QrCodeIcon from '@/assets/QrCodeIcon.svg';
           </div>
           <img src="@/assets/QRCodeLowDetail.png" class="test" :width="160"/>
           <div class="flex items-center justify-center h-fit-content flex-col">
-            <span class="text-lg font-bold">
+            <span class="hidden [@media(min-height:650px)]:flex text-lg font-bold">
               <span class="tablet:hidden">Cliquez ici pour contribuer</span>
               <span class="hidden tablet:block">Cliquez ou scannez pour contribuer</span>
             </span>
--- src/view-component/presentation/sections/header/HeaderSmallHeight.vue
+++ src/view-component/presentation/sections/header/HeaderSmallHeight.vue
@@ -4,6 +4,8 @@ import QrCode from '@/view-component/component/QrCode.vue';
 import ShouldScrollIndicator from '@/components/indicator/ShouldScrollIndicator.vue';
 import ListStatCard from '@/view-component/component/ListStatCard.vue';
 import { ref } from 'vue';
+import Timeline from '@/view-component/component/Timeline.vue';
+import Video from '@/view-component/component/Video.vue';
 
 const refListStatCard = ref<HTMLElement | null>(null);
 
@@ -11,7 +13,6 @@ function scrollToListStatCard() {
   if (!refListStatCard.value) return;
   refListStatCard.value.scrollIntoView({ behavior: 'smooth' });
 }
-
 </script>
 <template>
   <div
@@ -19,7 +20,7 @@ function scrollToListStatCard() {
       w-full h-full
       flex flex-col items-center
     ">
-    <div class="h-1/2 w-full flex flex-col gap-2 items-center justify-around py-2">
+    <div class="min-h-1/2 w-full flex flex-col gap-6 items-center justify-around py-2">
       <Title>
         <template #title>
           <div class="flex items-center gap-2">
@@ -28,13 +29,21 @@ function scrollToListStatCard() {
             <span class="text-primary">Construisons notre mosquée</span>
           </div>
         </template>
-        <template #subtitle>
-          <div class="[@media(--hsm)]:hidden">
-            Merci pour votre générosité et votre soutien à cette noble cause
-          </div>
-        </template>
       </Title>
-      <QrCode/>
+      <div class="grow flex flex-row gap-4 w-full ">
+        <QrCode />
+        <Video
+          youtubeId="QLT7Je6up_w"
+        >
+      </Video>
+        <!-- <iframe 
+          src="https://www.youtube.com/embed/QLT7Je6up_w" 
+          title="Le Récap : La mosquée de Vitry, c&#39;est signé !" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen/> -->
+      </div>
       <ShouldScrollIndicator 
         :shouldScroll="true"
         :scrollDirection="'down'"
@@ -42,17 +51,14 @@ function scrollToListStatCard() {
       />
     </div>
     <div 
-    class="w-full h-1/2 phone:max-w-3/4 tablet:max-w-1/2" 
-    ref="refListStatCard">
+      class="w-full grow phone:max-w-3/4" 
+      ref="refListStatCard">
       <ListStatCard 
-        class="w-full h-full items-center gap-x-4"
-        class-stat-card-1="col-span-2 phone:col-span-1"
+        class="w-full h-full items-center gap-x-16"
+        class-stat-card-1="col-span-2 tablet:col-span-1"
         class-stat-card-2="col-span-2 phone:col-span-1"
-        class-stat-card-3="col-span-2 phone:col-span-1"
-        class-stat-card-4="col-span-2 phone:col-span-1"
-      >
-        <!-- <div class="hidden phone:flex col-span-2 bg-primary self-stretch">
-        </div> -->
+        class-stat-card-3="col-span-2"
+        class-stat-card-4="col-span-2">
       </ListStatCard>
     </div>
   </div>
