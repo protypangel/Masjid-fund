8b3e5780fd1664287d28e8142abb4cd48ccb62fc
protypangel <mascret@et.esiea.fr>
2025-08-27 19:22:22 +0200

    use vite plugin vue devtool ?

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,15 @@ import vue from '@vitejs/plugin-vue'
 import tailwind from '@tailwindcss/vite'
 import svgLoader from 'vite-svg-loader'
 import { fileURLToPath, URL } from 'node:url'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwind(), svgLoader()],
+  plugins: [
+    vue(), 
+    tailwind(), 
+    svgLoader()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
