a3bae41e6cdc66fd8e6039b653c4eb5d01895ea1
protypangel <mascret@et.esiea.fr>
2025-08-31 00:08:47 +0200

    Change Filename Page

--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const PresentationPage = () => import('./views/PresentationPage.vue')
-const RecapPage = () => import('./views/RecapPage.vue')
+const HomePage = () => import('./views/HomePage.vue')
+const SuiviPage = () => import('./views/SuiviPage.vue')
 
 export const router = createRouter({
 	history: createWebHistory(),
 	routes: [
-		{ path: '/', name: 'home', component: PresentationPage, meta: { index: 0} },
-		{ path: '/recap', name: 'recap', component: RecapPage, meta: { index: 1} },
+		{ path: '/', name: 'home', component: HomePage, meta: { index: 0} },
+		{ path: '/recap', name: 'recap', component: SuiviPage, meta: { index: 1} },
 	],
-	scrollBehavior() {
-		return { top: 0 }
-	}
 })
 
 export default router 
